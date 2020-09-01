0acac87acf1778d0c800714e80f450f1329c505c
Lanza <lanzathm@gmail.com>
2020-09-01 17:14:00 -0300

    Header done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template>
   <div id="app">
-    <router-view />
+    <div class="container grid-lg pt-2">
+      <header class="navbar">
+        <section class="navbar-section">
+          <router-link to="/" class="btn btn-link">Home</router-link>
+          <router-link to="/kanban" class="btn btn-link">Kanban</router-link>
+        </section>
+        <section class="navbar-center">
+           <!-- Webpack sets src folder as @ -->
+          <img class="img-responsive img-logo" src="@/assets/todoLogo.png" alt="Logo" />
+        </section>
+        <section class="navbar-section">
+          <a href="https://github.com/lanzath" class="btn btn-link">Github</a>
+          <a href="https://www.linkedin.com/in/mt-lanza/" class="btn btn-link">LinkedIn</a>
+        </section>
+      </header>
+
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'app',
+    name: 'app'
   }
 </script>
+
+<style scoped>
+  .img-logo {
+      max-width: 100px;
+    }
+</style>

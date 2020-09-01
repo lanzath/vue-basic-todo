ac7d65726f2731075593fbc8dce63e044c7405c1
Lanza <lanzathm@gmail.com>
2020-09-01 16:49:44 -0300

    View rendering with router-view

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,11 @@
 <template>
   <div id="app">
-    <div class="container grid-xs py-2">
-      <!-- Webpack sets src folder as @ -->
-      <img class="img-responsive img-logo" src="@/assets/todoLogo.png" alt="Logo">
-
-      <!-- Submit and prevent default event -->
-      <form @submit.prevent="addTodo(todo)">
-        <div class="input-group py-2">
-          <input
-            type="text"
-            v-model="todo.description"
-            class="form-input"
-            placeholder="Nova tarefa"
-          >
-          <button class="btn btn-primary input-group-btn">Adicionar</button>
-        </div>
-      </form>
-
-      <div class="todo-list">
-        <Todo
-          v-for="todo in todos"
-          :key="todo.id"
-          @toggle="toggleTodo"
-          @remove="removeTodo"
-          :todo="todo"
-        />
-      </div>
-    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-  import Todo from './components/Todo';
-
   export default {
     name: 'app',
-    components: { Todo },
-    data() {
-      return { todos: [], todo: { checked: false } };
-    },
-    methods: {
-      addTodo(todo) {
-        todo.id = Date.now();
-        this.todos.push(todo);
-        this.todo = { checked: false };
-      },
-
-      toggleTodo(todo) {
-        const index = this.todos.findIndex(item => item.id === todo.id);
-        if (index > -1) {
-          const checked = !this.todos[index].checked;
-          this.$set(this.todos, index, {...this.todos[index], checked})
-        }
-      },
-
-      removeTodo(todo) {
-        const index = this.todos.findIndex(item => item.id === todo.id);
-        if (index > -1) {
-          this.$delete(this.todos, index);
-        }
-      }
-    }
   }
 </script>
-
-<style scoped>
-  .img-logo {
-    max-width: 200px;
-    margin: 0 auto;
-  }
-
-  .todo-list {
-    padding-top: 2rem;
-  }
-</style>

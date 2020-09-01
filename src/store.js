e14517c8f1f4300254770cda903ed495bd4e7a4f
Lanza <lanzathm@gmail.com>
2020-09-01 18:01:17 -0300

    Vuex Store

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+// payload is the new count value
+const store = new Vuex.Store({
+  state: { count: 0 },
+  mutations: {
+    setCount(state, payload) {
+      state.count = payload;
+    }
+  }
+});
+
+export default store;

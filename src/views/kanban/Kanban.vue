2176050504209396682627961aebc5b2d985fa90
Lanza <lanzathm@gmail.com>
2020-09-01 18:44:22 -0300

    Kanban page under construction

---
+++ src/views/kanban/Kanban.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Board
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'kanban',
+  computed: {
+    //
+  },
+  methods: {
+   //
+  }
+}
+</script>
+
+<style>
+
+</style>

2dfca9200d36f6d06ead44b70c944980b17ebbf8
Lanza <lanzathm@gmail.com>
2020-09-03 22:14:35 -0300

    Todo kanban board layout done

--- src/views/kanban/Kanban.vue
+++ src/views/kanban/Kanban.vue
@@ -1,21 +1,56 @@
 <template>
-  <div>
-    Board
+  <div class="container-board">
+    <div class="columns">
+      <div class="column col-5">
+        <div class="panel">
+          <div class="panel-header">
+            <div class="panel-title">A fazer</div>
+          </div>
+          <div class="panel-body">
+            {{ todos }}
+          </div>
+          <div class="panel-footer">
+            <!-- buttons or inputs -->
+          </div>
+        </div>
+      </div>
+      <div class="column col-5">
+        <div class="panel">
+          <div class="panel-header">
+            <div class="panel-title">Concluídos</div>
+          </div>
+          <div class="panel-body">
+            <!-- contents -->
+          </div>
+          <div class="panel-footer">
+            <!-- buttons or inputs -->
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'kanban',
-  computed: {
-    //
-  },
-  methods: {
-   //
-  }
+  //
 }
 </script>
 
-<style>
+<style scoped>
+.container-board {
+  height: calc(100vh - 100px);
+  margin-top: 8px;
+  padding: 10px;
+}
 
+.columns {
+  height: 100%;
+}
+
+.columns .column .panel {
+  height: 100%;
+  border: none;
+  box-shadow: 0 0.25rem 1rem rgba(48, 55, 66, .2);
+}
 </style>

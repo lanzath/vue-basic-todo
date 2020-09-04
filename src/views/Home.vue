9282e1e1076d5719d73f944d0dc2ae768f17b538
Lanza <lanzathm@gmail.com>
2020-09-03 21:29:45 -0300

    Refactored code to use Vuex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,37 +32,28 @@ export default {
   name: 'Home',
   components: { Todo },
   data() {
-      return { todo: { checked: false }, loading: false };
+      return { todo: { checked: false } };
     },
     computed: {
       todos() {
         return this.$store.state.todos;
+      },
+      loading() {
+        return this.$store.state.loading;
       }
     },
     methods: {
       async addTodo(todo) {
-        try {
-          this.loading = true;
-          await this.$store.dispatch('addTodo', todo);
-          this.todo = { checked: false };
-        } finally {
-          this.loading = false;
-        }
+        await this.$store.dispatch('addTodo', todo);
+        this.todo = { checked: false }
       },
 
       toggleTodo(todo) {
-        const index = this.todos.findIndex(item => item.id === todo.id);
-        if (index > -1) {
-          const checked = !this.todos[index].checked;
-          this.$set(this.todos, index, {...this.todos[index], checked})
-        }
+        this.$store.dispatch('toggleTodo', todo);
       },
 
       removeTodo(todo) {
-        const index = this.todos.findIndex(item => item.id === todo.id);
-        if (index > -1) {
-          this.$delete(this.todos, index);
-        }
+        this.$store.dispatch('removeTodo', todo);
       }
     }
 }

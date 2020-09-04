8ef543693ddedef138903c2d62f57187cc2b3cac
Lanza <lanzathm@gmail.com>
2020-09-03 21:29:33 -0300

    Using vuex to manage actions of Todos

--- src/store.js
+++ src/store.js
@@ -4,26 +4,53 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 // todos is an empty array as initial state
-const state = {todos: []};
+const state = { todos: [], loading: false };
 
-// Action to simulate API call
+// Actions handle the commit mutations
 const actions = {
   // using setTimeout to simulate wait time of API call
   addTodo({commit}, todo) {
+    commit('setLoading', true)
     return new Promise(resolve => {
       setTimeout(() => {
         todo.id = Date.now()
         commit('addTodo', todo)
+        commit('setLoading', false)
         resolve(todo)
-      }, 1000)
+      }, 500)
     })
+  },
+
+  toggleTodo({ commit }, todo) {
+    commit('toggleTodo', todo)
+  },
+
+  removeTodo({ commit }, todo) {
+    commit('removeTodo', todo)
   }
 }
 
-// payload is the state new value
+// Payload is the state new value
+// Mutations will handle the state changes
 const mutations = {
   addTodo(state, payload) {
     state.todos.push(payload)
+  },
+
+  setLoading(state, payload) {
+    state.loading = payload
+  },
+
+  toggleTodo(state, payload) {
+    const index = state.todos.findIndex(item => item.id === payload.id);
+    if (index > -1) {
+      const checked = !state.todos[index].checked;
+      Vue.set(state.todos, index, {...state.todos[index], checked})
+    }
+  },
+
+  removeTodo(state, payload) {
+    state.todos = state.todos.filter(filteredTodos => filteredTodos.id !== payload.id)
   }
 }
 
